855f0fe5fa615516d847b3100d9df9df6b9f3773
Vladi Leshko <leshkovladik@gmail.com>
2022-08-20 10:48:52 +0300

    added modal windows

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,30 @@
+//burger menu script
 function menuOnClick() {
     document.getElementById("menu-bar").classList.toggle("change");
     document.getElementById("nav").classList.toggle("change");
     document.getElementById("menu-bg").classList.toggle("change-bg");
+};
+
+//modal window script
+var modal = document.getElementById("modalWindow");
+var topBtn = document.getElementById("topPhoneButton");
+var bottomBtn = document.getElementById("bottomPhoneButton");
+var span = document.getElementsByClassName("closeButton")[0];
+
+topBtn.onclick = function() {
+    modal.style.display = "block";
+};
+
+bottomBtn.onclick = function() {
+    modal.style.display = "block";
+};
+
+span.onclick = function() {
+    modal.style.display = "none";
+};
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
 };
